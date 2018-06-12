1b77b8cb221b4522eb74c3e7611c008f600c930c
luckydonald <luckydonald@flutterb.at>
2018-06-12 10:17:48 +0200

    initial commit.

---
+++ SelfhostedMouse/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from luckydonaldUtils.logger import logging
+
+from SelfhostedMouse.webserver import web_socket, web_server
+from SelfhostedMouse.mouse import mouse
+
+BIND_IP = '0.0.0.0'
+
+__author__ = 'luckydonald'
+logger = logging.getLogger(__name__)
+logging.add_colored_handler(level=logging.INFO)
+
+
+
+# WS server that sends messages at random intervals
+import asyncio
+import websockets
+
+
+
+
+
+def handle_read(loop, conn, waiter):
+    req = conn.recv(1024)
+    loop.remove_reader(conn.fileno())
+    waiter.set_result(req)
+
+
+sock = web_socket(BIND_IP , 8080)
+loop = asyncio.get_event_loop()
+logger.debug('registering web_server')
+loop.create_task(web_server(loop, sock))
+try:
+    loop.run_until_complete(websockets.serve(mouse, BIND_IP, 6789))
+finally:
+    loop.run_forever()
+    loop.close()
+# end def
+
+
+
+#asyncio.get_event_loop().run_until_complete(
+#    websockets.serve(mouse, 'localhost', 6789))
+#asyncio.get_event_loop().run_forever()
---
+++ SelfhostedMouse/mouse.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import json
+from pynput.mouse import Controller, Button
+from luckydonaldUtils.logger import logging
+
+__author__ = 'luckydonald'
+logger = logging.getLogger(__name__)
+
+BUTTONS = {
+    'left': Button.left, 'right': Button.right, 'middle': Button.middle,
+    1: Button.left, 2: Button.right, 3: Button.middle
+}
+
+USERS = set()
+m = Controller()
+
+async def register(websocket):
+    logger.debug('called.')
+    USERS.add(websocket)
+
+
+async def unregister(websocket):
+    logger.debug('called.')
+    USERS.remove(websocket)
+
+
+async def mouse(websocket, _):
+    # register(websocket) sends user_event() to websocket
+    logger.debug('called.')
+    await register(websocket)
+    try:
+        # await websocket.send(state_event())
+        async for message in websocket:
+            logging.debug('message: {}'.format(message))
+            data = json.loads(message)
+            if data['action'] == 'click' and 'button' in data and data['button'] in BUTTONS:
+                button = data['button']
+                logger.info('click - button: {}'.format(button))
+                m.click(BUTTONS[button])
+            elif data['action'] == 'move' and 'x' in data and 'y' in data:
+                x, y = data['x'], data['y']
+                logger.info('move - x: {x}, y: {y}'.format(x=y, y=y))
+                m.move(x,y)
+            elif data['action'] == 'scroll' and 'x' in data and 'y' in data:
+                x, y = int(data['x']), int(data['y'])
+                logger.info('scroll - x: {x}, y: {y}'.format(x=x, y=y))
+                m.scroll(x,y)
+            else:
+                logging.error("unsupported event: {!r}".format(data))
+    finally:
+        await unregister(websocket)
